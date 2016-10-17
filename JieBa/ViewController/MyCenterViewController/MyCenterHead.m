dcf5580c4c44b3590042b6404a0eb2cf6ba1e831
wangy322@sina.com <wangyang@wangyangdemacbook-air.local>
2016-10-17 23:02:13 +0800

    wy

--- JieBa/ViewController/MyCenterViewController/MyCenterHead.m
+++ JieBa/ViewController/MyCenterViewController/MyCenterHead.m
@@ -10,6 +10,13 @@
 
 @interface MyCenterHead()
 @property(nonatomic,strong)UIImageView *bgImageView;
+@property(nonatomic,strong)UIButton *userImgBtn;
+@property(nonatomic,strong)UILabel *nameLabel;
+@property(nonatomic,strong)UILabel *levelLabel;
+@property(nonatomic,strong)UIView *footerView;
+@property(nonatomic,strong)UIImageView *cutLine;
+@property(nonatomic,strong)UIButton *signBtn;
+@property(nonatomic,strong)UIButton *activeBtn;
 @end
 
 @implementation MyCenterHead
@@ -17,6 +24,13 @@
 -(instancetype)initWithFrame:(CGRect)frame{
     if(self = [super initWithFrame:frame]){
         [self bgImageView];
+        [self userImgBtn];
+        [self nameLabel];
+        [self levelLabel];
+        [self footerView];
+        [self cutLine];
+        [self signBtn];
+        [self activeBtn];
     }
     return self;
 }
@@ -30,5 +44,92 @@
         _bgImageView = bgImageView;
     }
     return _bgImageView;
+}
+
+- (UIButton *)userImgBtn {
+    if (!_userImgBtn) {
+        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(WidthXiShu(10), HeightXiShu(60), WidthXiShu(65), HeightXiShu(65))];
+        btn.layer.cornerRadius = btn.halfWidth;
+        btn.layer.borderWidth = 5;
+        btn.layer.borderColor = RGBACOLOR(0, 0, 0, .3).CGColor;
+        
+        //[btn setImage:[GetImagePath getImagePath:@"个人中心9"] forState:UIControlStateNormal];
+        [btn setImageEdgeInsets:UIEdgeInsetsMake(5, 5, 5, 5)];
+        [self addSubview:btn];
+        
+        _userImgBtn = btn;
+    }
+    return _userImgBtn;
+}
+
+-(UILabel *)nameLabel{
+    if(!_nameLabel){
+        UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(WidthXiShu(90), HeightXiShu(72), WidthXiShu(150), HeightXiShu(20))];
+        nameLabel.text = @"孙大蛇";
+        nameLabel.textColor = [UIColor whiteColor];
+        nameLabel.font = HEITI(HeightXiShu(14));
+        [self addSubview:nameLabel];
+        _nameLabel = nameLabel;
+    }
+    return _nameLabel;
+}
+
+-(UILabel *)levelLabel{
+    if(!_levelLabel){
+        UILabel *levelLabel = [[UILabel alloc] initWithFrame:CGRectMake(WidthXiShu(90), HeightXiShu(97), WidthXiShu(150), HeightXiShu(20))];
+        levelLabel.textColor = [UIColor whiteColor];
+        levelLabel.text = @"普通会员";
+        levelLabel.font = HEITI(HeightXiShu(14));
+        [self addSubview:levelLabel];
+        _levelLabel = levelLabel;
+    }
+    return _levelLabel;
+}
+
+-(UIView *)footerView{
+    if(!_footerView){
+        UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, HeightXiShu(165), kScreenWidth, HeightXiShu(45))];
+        footerView.backgroundColor = NavColor;
+        footerView.alpha = .3;
+        [self addSubview:footerView];
+        _footerView = footerView;
+    }
+    return _footerView;
+}
+
+-(UIImageView *)cutLine{
+    if(!_cutLine){
+        UIImageView *cutLine = [[UIImageView alloc] initWithFrame:CGRectMake(kScreenWidth/2-1, HeightXiShu(165), WidthXiShu(2), HeightXiShu(45))];
+        cutLine.backgroundColor = NavColor;
+        [self addSubview:cutLine];
+        _cutLine = cutLine;
+    }
+    return _cutLine;
+}
+
+-(UIButton *)signBtn{
+    if(!_signBtn){
+        UIButton *signBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        signBtn.frame = CGRectMake(0, HeightXiShu(165), kScreenWidth/2-2, HeightXiShu(45));
+        [signBtn setTitle:@"签到" forState:UIControlStateNormal];
+        [signBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        signBtn.titleLabel.font = HEITI(HeightXiShu(14));
+        [self addSubview:signBtn];
+        _signBtn = signBtn;
+    }
+    return _signBtn;
+}
+
+-(UIButton *)activeBtn{
+    if(!_activeBtn){
+        UIButton *activeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        activeBtn.frame = CGRectMake(kScreenWidth/2+2, HeightXiShu(165), kScreenWidth/2-2, HeightXiShu(45));
+        [activeBtn setTitle:@"邀请" forState:UIControlStateNormal];
+        [activeBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        activeBtn.titleLabel.font = HEITI(HeightXiShu(14));
+        [self addSubview:activeBtn];
+        _activeBtn = activeBtn;
+    }
+    return _activeBtn;
 }
 @end
--- JieBa/ViewController/MyCenterViewController/MyCenterViewController.m
+++ JieBa/ViewController/MyCenterViewController/MyCenterViewController.m
@@ -11,6 +11,7 @@
 
 @interface MyCenterViewController ()
 @property(nonatomic,strong)MyCenterHead *headView;
+-(UIView *)cellViewWithMainImg:(NSString *)mainImgName mainText:(NSString *)mainText subText:(NSString *)subText buttonName1:(NSString *)buttonName1 buttonName2:(NSString *)buttonName2 selector1:(SEL)selector1 selector2:(SEL)selector2 stingOrImage:(BOOL)stingOrImage;
 @end
 
 @implementation MyCenterViewController
@@ -36,7 +37,7 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     if(section == 0 || section == 1 || section == 2){
-        return 2;
+        return 1;
     }else if (section == 3){
         return 4;
     }else{
@@ -45,17 +46,17 @@
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    if(indexPath.section == 0){
-        return HeightXiShu(196);
+    if(indexPath.section == 0 || indexPath.section == 1 || indexPath.section == 2){
+        return HeightXiShu(80);
     }
-    return HeightXiShu(100);
+    return HeightXiShu(40);
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    if(section == 1){
-        return HeightXiShu(22);
+    if(section == 0){
+        return 0;
     }
-    return 0;
+    return HeightXiShu(22);
 }
 
 //- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
@@ -84,24 +85,33 @@
 //    return view;
 //}
 
-//-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-//    if(indexPath.section == 0){
-//        MainButtonCell* cell=[tableView dequeueReusableCellWithIdentifier:@"MainButtonCell"];
-//        if (!cell) {
-//            cell=[[MainButtonCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"MainButtonCell"];
-//        }
-//        cell.selectionStyle = UITableViewCellSelectionStyleNone;
-//        return cell;
-//    }else{
-//        InvitationCell* cell=[tableView dequeueReusableCellWithIdentifier:@"InvitationCell"];
-//        if (!cell) {
-//            cell=[[InvitationCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"InvitationCell"];
-//        }
-//        cell.selectionStyle = UITableViewCellSelectionStyleNone;
-//        cell.modelsArr = self.invitationModels;
-//        return cell;
-//    }
-//}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString* const identifier = @"cell";
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    [cell.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    if(indexPath.section == 0 || indexPath.section == 1 || indexPath.section == 2){
+        NSString *imgName = @[@"myCenter_ balance",@"myCenter_ order",@"myCenter_center"][indexPath.section];
+        NSString *mainText = @[@"账户余额",@"我的订单",@"个人中心"][indexPath.section];
+        NSString *buttonName1 = @[@"充值",[NSString stringWithFormat:@"待评价  %d",0],@"实名认证"][indexPath.section];
+        NSString *buttonName2 = @[@"体现",@"已评价",@"绑定银行卡"][indexPath.section];
+        NSString *selector1 = @[@"creditAction",@"evaluateAction",@"approveAction"][indexPath.section];
+        NSString *selector2 = @[@"withdrawAction",@"evaluateEndAction",@"bindAction"][indexPath.section];
+        
+        BOOL isString = nil;
+        if(indexPath.section == 0){
+            isString = YES;
+        }else{
+            isString = NO;
+        }
+        UIView *view = [self cellViewWithMainImg:imgName mainText:mainText subText:@"1000.00元" buttonName1:buttonName1 buttonName2:buttonName2 selector1:NSSelectorFromString(selector1) selector2:NSSelectorFromString(selector2) stingOrImage:isString];
+        [cell addSubview:view];
+    }
+    return cell;
+}
 
 #pragma mark - 页面元素
 -(MyCenterHead *)headView{
@@ -112,4 +122,81 @@
     return _headView;
 }
 
+#pragma mark - 创建前3个view
+-(UIView *)cellViewWithMainImg:(NSString *)mainImgName mainText:(NSString *)mainText subText:(NSString *)subText buttonName1:(NSString *)buttonName1 buttonName2:(NSString *)buttonName2 selector1:(SEL)selector1 selector2:(SEL)selector2 stingOrImage:(BOOL)stingOrImage{
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, HeightXiShu(80))];
+    UIView *cutLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, .5)];
+    cutLine.backgroundColor = AllLightGrayColor;
+    [view addSubview:cutLine];
+    [cutLine setMaxX:view.width maxY:view.halfHeight];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(WidthXiShu(15), HeightXiShu(10), WidthXiShu(19), HeightXiShu(19))];
+    imageView.image = [GetImagePath getImagePath:mainImgName];
+    [view addSubview:imageView];
+    
+    UILabel *mainLabel = [[UILabel alloc] initWithFrame:CGRectMake(WidthXiShu(53), 0, WidthXiShu(150), HeightXiShu(40))];
+    mainLabel.font = HEITI(HeightXiShu(14));
+    mainLabel.text = mainText;
+    mainLabel.textColor = TitleColor;
+    [view addSubview:mainLabel];
+    
+    UILabel *subLabel = [[UILabel alloc] initWithFrame:CGRectMake(kScreenWidth-WidthXiShu(10)-WidthXiShu(100), 0, WidthXiShu(100), HeightXiShu(40))];
+    subLabel.font = HEITI(HeightXiShu(14));
+    subLabel.text = subText;
+    subLabel.textColor = TitleColor;
+    subLabel.hidden  = !stingOrImage;
+    subLabel.textAlignment = NSTextAlignmentRight;
+    [view addSubview:subLabel];
+    
+    UIImageView *arrowImgView = [[UIImageView alloc] initWithFrame:CGRectMake(kScreenWidth-WidthXiShu(16), HeightXiShu(15), WidthXiShu(6), HeightXiShu(10))];
+    arrowImgView.backgroundColor = [UIColor purpleColor];
+    arrowImgView.hidden  = stingOrImage;
+    [view addSubview:arrowImgView];
+    
+    UIButton *button1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button1 setTitle:buttonName1 forState:UIControlStateNormal];
+    [button1 setTitleColor:TitleColor forState:UIControlStateNormal];
+    button1.titleLabel.font = HEITI(HeightXiShu(14));
+    button1.frame = CGRectMake(0, HeightXiShu(40), kScreenWidth/2-1, HeightXiShu(40));
+    [button1 addTarget:self action:selector1 forControlEvents:UIControlEventTouchUpInside];
+    [view addSubview:button1];
+    
+    UIButton *button2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button2 setTitle:buttonName2 forState:UIControlStateNormal];
+    [button2 setTitleColor:TitleColor forState:UIControlStateNormal];
+    button2.titleLabel.font = HEITI(HeightXiShu(14));
+    button2.frame = CGRectMake(kScreenWidth/2+1, HeightXiShu(40), kScreenWidth/2-1, HeightXiShu(40));
+    [button2 addTarget:self action:selector2 forControlEvents:UIControlEventTouchUpInside];
+    [view addSubview:button2];
+    
+    UIImageView *cutLine2 = [[UIImageView alloc] initWithFrame:CGRectMake(kScreenWidth/2, HeightXiShu(40), .5, HeightXiShu(40))];
+    cutLine2.backgroundColor = AllLightGrayColor;
+    [view addSubview:cutLine2];
+    return view;
+}
+
+#pragma mark - selector
+-(void)creditAction{
+    NSLog(@"creditAction");
+}
+
+-(void)withdrawAction{
+    NSLog(@"withdrawAction");
+}
+
+-(void)evaluateAction{
+    NSLog(@"evaluateAction");
+}
+
+-(void)evaluateEndAction{
+    NSLog(@"evaluateEndAction");
+}
+
+-(void)approveAction{
+    NSLog(@"approveAction");
+}
+
+-(void)bindAction{
+    NSLog(@"bindAction");
+}
 @end
